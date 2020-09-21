9855bd6c56f2a6263d572460b3fcee1dc2c23339
rexholmstrom <rexholmstrom96@gmail.com>
2020-09-21 22:18:59 +0200

    update

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("./public"));
 
-app.post("/url/:id", (req, res) => {});
+app.get("/url/:id", (req, res) => {});
 
 app.get("/:id", (req, res) => {});
 
